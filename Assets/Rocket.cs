3ff3ba8eb97438b47300e57a8d868d8b287cfe42
Eric Marty <contact@ericmarty.co>
2017-10-30 23:04:58 -0700

    Ship built with basic input and thrust

---
+++ Assets/Rocket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right");
+        }
+    }
+}
