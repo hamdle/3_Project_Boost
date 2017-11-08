a58e72097621773b7d6cb3c54584c93c665ed07c
Eric Marty <contact@ericmarty.co>
2017-11-07 22:25:33 -0800

    Stage and stage skip

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -141,7 +141,13 @@ public class Rocket : MonoBehaviour
 
     private void LoadNextLevel()
     {
-        SceneManager.LoadScene(1);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex % SceneManager.sceneCountInBuildSettings == 0)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     private void LoadFirstLevel()
